ceee592282837cbca45c85aaf278af2e716725b4
olhapoberezhets <poberezhets94@gmail.com>
2021-11-09 10:59:39 +0200

    Quizzler

---
+++ android/app/src/main/kotlin/com/poberezhetsolha/quizzler_new/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.poberezhetsolha.quizzler_new
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/question.dart
@@ -0,0 +1,8 @@
+import 'quiz_brain.dart';
+
+class Question {
+  String? questionText;
+  bool? questionAnswer;
+
+  Question(this.questionText, this.questionAnswer);
+}
